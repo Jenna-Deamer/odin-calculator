4be3fb4817ff27320952a2252faf9d1bfa378e8a
Jenna-Deamer <jennadeamer@gmail.com>
2024-12-29 12:41:51 -0500

    Add HTML calculator buttons & layout with CSS

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// basic math functions
 
+// operation variables
+let num1;
+let num2;
+let operation;
+
+// basic math functions
 function add (num1, num2){
     return num1 + num2;
 }
@@ -14,4 +19,8 @@ function multiply (num1, num2){
 
 function divide (num1, num2){
     return num1 / num2;
+}
+
+function operate (operation, num1, num2){
+ 
 }
