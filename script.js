b39381f96239fa50f0893ee43a44fdb3a7922532
Jenna Deamer <jennadeamer@gmail.com>
2025-01-03 22:10:14 -0500

    Add function to display multiple numbers

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// Get all buttons
-const buttons = document.querySelectorAll('.btn')
-// Attach event listeners 
-buttons.forEach(function(currentBtn){
-    currentBtn.addEventListener('click',function(){
-        console.log("Clicked " + currentBtn.value)
-    })
-})
-
+const legalOperands = ['0','1','2','3','4','5','6','7','8','9'];
+const legalOperators = ['+','-','*','/', '='];
+const decimal = '.';
+const display = document.querySelector('.display');
+// Hold user inputs for displaying
+let firstNumberList = [];
+let secondNumberList = [];
 // operation variables
 let num1;
 let num2;
 let operator = "";
+// Get all buttons
+const operands = document.querySelectorAll('.operand');
+const operators = document.querySelectorAll('.operator');
+// Attach event listeners 
+operands.forEach(function(currentBtn){
+    currentBtn.addEventListener('click',function(){
+        display.textContent = currentBtn.value;
+        firstNumberList.push(currentBtn.value);
+        // combine them ex 8,2 82
+        let firstNumber = firstNumberList.join('');
+        display.textContent = firstNumber;     
+
+        // somehow take all numbers pressed and combine them ex 8,2 82. Until operator or clear clicked
+        // Listen for operator or clear
+        // listen for numbers and combine them until = or clear is clicked
+        // Display legal arguments as user adds them
+
+        // add to array if legal stripping out commas and combining them until op clicked
+        // Add op to display
+        // listen for numbers again stripping out commas and turning them into a whole number displaying them on the screen until = or clear is clicked
+     
+    })
+})
 
 // basic math functions
 function add (num1, num2){
@@ -58,5 +79,5 @@ function populateDisplay (){
 }
 
 function clearDisplay (){
-
+    
 }
