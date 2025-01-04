f01e19d35a7127bc7180a8465c85082c329f358a
Jenna Deamer <jennadeamer@gmail.com>
2025-01-04 11:08:51 -0500

    Add event listener for operators, Move display of numbers inside of populateDisplay()

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
-const legalOperands = ['0','1','2','3','4','5','6','7','8','9'];
-const legalOperators = ['+','-','*','/', '='];
-const decimal = '.';
-const display = document.querySelector('.display');
+const legalOperands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const legalOperators = ["+", "-", "*", "/", "="];
+const decimal = ".";
+const display = document.querySelector(".displayContents");
 // Hold user inputs for displaying
 let firstNumberList = [];
 let secondNumberList = [];
-// operation variables
+// Operation variables
 let num1;
 let num2;
-let operator = "";
-// Get all buttons
-const operands = document.querySelectorAll('.operand');
-const operators = document.querySelectorAll('.operator');
-// Attach event listeners 
-operands.forEach(function(currentBtn){
-    currentBtn.addEventListener('click',function(){
-        display.textContent = currentBtn.value;
-        firstNumberList.push(currentBtn.value);
-        // combine them ex 8,2 82
-        let firstNumber = firstNumberList.join('');
-        display.textContent = firstNumber;     
+// Define buttons
+const operands = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
 
-        // somehow take all numbers pressed and combine them ex 8,2 82. Until operator or clear clicked
-        // Listen for operator or clear
-        // listen for numbers and combine them until = or clear is clicked
-        // Display legal arguments as user adds them
+// Attach event listeners
+operands.forEach(function (currentBtn) {
+  currentBtn.addEventListener("click", function () {
+    firstNumberList.push(currentBtn.value);
+    // combine numbers ex 8,2 82
+    let firstNumber = firstNumberList.join("");
 
-        // add to array if legal stripping out commas and combining them until op clicked
-        // Add op to display
-        // listen for numbers again stripping out commas and turning them into a whole number displaying them on the screen until = or clear is clicked
-     
-    })
-})
+    populateDisplay(firstNumber);
+  });
+});
+
+operators.forEach(function (currentBtn) {
+  currentBtn.addEventListener("click", function () {
+    let operator = currentBtn.value;
+    console.log("Stored " + operator);
+    populateDisplay(operator);
+  });
+});
+
+function populateDisplay(firstNumber, operator, secondNumber) {
+  // if firstNumber is a number display
+  if (!isNaN(firstNumber)) {
+    // Get first Operand(s)
+    display.textContent = firstNumber;
+  }
+
+  if (!display.innerHTML === "") {
+    console.log(operator);
+    display.textContent = operator;
+  }
+
+  // if op selected listen for a operand for secondNumber operand
+  // if secondNumber has at least a operand in it ex 2 or 234 listen for =
+  // if firstNumber contains a number & operator contains an operator 
+  // if = clicked sum & store sum as firstNumber.
+}
+
+function clearDisplay() {}
 
 // basic math functions
-function add (num1, num2){
-    return num1 + num2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract (num1, num2){
-    return num1 - num2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply (num1, num2){
-    return num1 * num2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide (num1, num2){
-    return num1 / num2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-function operate ( num1, operator, num2){
-    if (operator === "+"){
-        add(num1, num2);
-    }
-    else if (operator === "-"){
-        subtract(num1, num2);
-    }
-    else if (operator === "*"){
-        multiply(num1, num2);
-    }
-    else if (operator === "/"){
-        divide(num1, num2);
-    }
-    else{
-        return "Error invalid or missing operator"
-    }
-}
-
-// Need to get what value of what user clicked on.
-// Validate its a legal agr. Ex entering an op first should not do anything
-// Push number to display & listen for next input
-// Push op to display listen for final number
-// Listen for = if pressed early don't do anything
-// Display result and listen for next op press, backspace or clear
-function populateDisplay (){
-
-}
-
-function clearDisplay (){
-    
+function operate(num1, operator, num2) {
+  if (operator === "+") {
+    add(num1, num2);
+  } else if (operator === "-") {
+    subtract(num1, num2);
+  } else if (operator === "*") {
+    multiply(num1, num2);
+  } else if (operator === "/") {
+    divide(num1, num2);
+  } else {
+    return "Error invalid or missing operator";
+  }
 }
