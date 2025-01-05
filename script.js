0753609fd9638a9958137e828ba51fbab31556d9
Jenna Deamer <jennadeamer@gmail.com>
2025-01-04 19:07:13 -0500

    Remove if statements, Refactor event listeners, Add getNumber function to check what number to get value for, clean code

--- script.js
+++ script.js
@@ -1,83 +1,95 @@
-const legalOperands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const legalOperators = ["+", "-", "*", "/", "="];
 const decimal = ".";
 const display = document.querySelector(".displayContents");
 // Hold user inputs for displaying
 let firstNumberList = [];
 let secondNumberList = [];
-// Operation variables
-let num1;
-let num2;
+// define operation vars
+let num1 = "";
+let operator = "";
+let num2 = "";
 // Define buttons
 const operands = document.querySelectorAll(".operand");
 const operators = document.querySelectorAll(".operator");
+const clearButton = document
+    .querySelector(".clear-btn")
+    .addEventListener("click", clearDisplay);
+const equalButton = document.querySelector(".equal-btn");
 
-// Attach event listeners
 operands.forEach(function (currentBtn) {
-  currentBtn.addEventListener("click", function () {
-    firstNumberList.push(currentBtn.value);
-    // combine numbers ex 8,2 82
-    let firstNumber = firstNumberList.join("");
-
-    populateDisplay(firstNumber);
-  });
+    currentBtn.addEventListener("click", function () {
+        // if operator has value listen for num2
+        if (operator !== "") {
+            num2 = getNumber(num2, secondNumberList, currentBtn.value);
+            console.log("num2 " + num2)
+        }
+        // if operator doesn't have value, listen for num1
+        else {
+            num1 = getNumber(num1, firstNumberList, currentBtn.value);
+            console.log("num1 " + num1)
+        }
+    });
 });
 
 operators.forEach(function (currentBtn) {
-  currentBtn.addEventListener("click", function () {
-    let operator = currentBtn.value;
-    console.log("Stored " + operator);
-    populateDisplay(operator);
-  });
+    currentBtn.addEventListener("click", function () {
+        if (num1 !== "") {
+            operator = currentBtn.value;
+            updateOperatorDisplay(operator);
+        }
+    });
 });
 
-function populateDisplay(firstNumber, operator, secondNumber) {
-  // if firstNumber is a number display
-  if (!isNaN(firstNumber)) {
-    // Get first Operand(s)
-    display.textContent = firstNumber;
-  }
-
-  if (!display.innerHTML === "") {
-    console.log(operator);
-    display.textContent = operator;
-  }
-
-  // if op selected listen for a operand for secondNumber operand
-  // if secondNumber has at least a operand in it ex 2 or 234 listen for =
-  // if firstNumber contains a number & operator contains an operator 
-  // if = clicked sum & store sum as firstNumber.
+function getNumber(numberValue, numberArray, buttonValue) {
+    numberArray.push(buttonValue);
+    // combine numbers ex 8,2 82
+    numberValue = numberArray.join("");
+    updateNumDisplay(numberValue);
+    return numberValue;
 }
 
-function clearDisplay() {}
+function updateNumDisplay(numberValue) {
+    display.textContent = numberValue;
+}
+function updateOperatorDisplay(operator) {
+    display.textContent = operator;
+}
 
+function clearDisplay() {
+    display.textContent = "";
+    // reset variables
+    num1 = "";
+    operator = "";
+    num2 = "";
+    firstNumberList = [];
+    secondNumberList = [];
+}
 // basic math functions
 function add(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  return num1 - num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+    return num1 / num2;
 }
 
 function operate(num1, operator, num2) {
-  if (operator === "+") {
-    add(num1, num2);
-  } else if (operator === "-") {
-    subtract(num1, num2);
-  } else if (operator === "*") {
-    multiply(num1, num2);
-  } else if (operator === "/") {
-    divide(num1, num2);
-  } else {
-    return "Error invalid or missing operator";
-  }
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "*") {
+        multiply(num1, num2);
+    } else if (operator === "/") {
+        divide(num1, num2);
+    } else {
+        return "Error invalid or missing operator";
+    }
 }
